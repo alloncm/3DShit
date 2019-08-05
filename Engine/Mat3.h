311f336c1adbcf0aa6d75d14c01fc2e1694de505
alloncm <alloncm@gmail.com>
2019-08-05 18:52:28 +0300

    fixes

--- Engine/Mat3.h
+++ Engine/Mat3.h
@@ -20,42 +20,42 @@
 ******************************************************************************************/
 #pragma once
 
-#include "Vec2.h"
+#include "Vec3.h"
 
 template <typename T>
-class _Mat2
+class _Mat3
 {
 public:
-	_Mat2& operator=( const _Mat2& rhs )
+	_Mat3& operator=(const _Mat3& rhs)
 	{
-		memcpy( elements,rhs.elements,sizeof( elements ) );
+		memcpy(elements, rhs.elements, sizeof(elements));
 		return *this;
 	}
-	_Mat2& operator*=( T rhs )
+	_Mat3& operator*=(T rhs)
 	{
-		for( auto& row : elements )
+		for (auto& row : elements)
 		{
-			for( T& e : row )
+			for (T& e : row)
 			{
 				e *= rhs;
 			}
 		}
 		return *this;
 	}
-	_Mat2 operator*( T rhs ) const
+	_Mat3 operator*(T rhs) const
 	{
-		_Mat2 result = *this;
+		_Mat3 result = *this;
 		return result *= rhs;
 	}
-	_Mat2 operator*( const _Mat2& rhs ) const
+	_Mat3 operator*(const _Mat3& rhs) const
 	{
-		_Mat2 result;
-		for( size_t j = 0; j < 2; j++ )
+		_Mat3 result;
+		for (size_t j = 0; j < 3; j++)
 		{
-			for( size_t k = 0; k < 2; k++ )
+			for (size_t k = 0; k < 3; k++)
 			{
 				T sum = (T)0.0;
-				for( size_t i = 0; i < 2; i++ )
+				for (size_t i = 0; i < 3; i++)
 				{
 					sum += elements[j][i] * rhs.elements[i][k];
 				}
@@ -64,44 +64,42 @@ public:
 		}
 		return result;
 	}
-	static _Mat2 Identity()
+	static _Mat3 Identity()
 	{
-		_Mat2 i = { (T)1.0,(T)0.0,(T)0.0,(T)1.0 };
-		return i;
+		return{
+			(T)1.0,(T)0.0,(T)0.0,
+			(T)0.0,(T)1.0,(T)0.0,
+			(T)0.0,(T)0.0,(T)1.0
+		};
 	}
-	static _Mat2 Rotation( T theta )
+	static _Mat3 Scaling(T factor)
 	{
-		const T cosTheta = cos( theta );
-		const T sinTheta = sin( theta );
-		_Mat2 r = { 
-			cosTheta,	sinTheta,
-			-sinTheta,	cosTheta };
-		return r;
-	}
-	static _Mat2 Scaling( T factor )
-	{
-		_Mat2 s = { factor,(T)0.0,(T)0.0,factor };
-		return s;
+		return{
+			factor,(T)0.0,(T)0.0,
+			(T)0.0,factor,(T)0.0,
+			(T)0.0,(T)0.0,factor
+		};
 	}
 public:
 	// [ row ][ col ]
-	T elements[2][2];
+	T elements[3][3];
 };
 
 template<typename T>
-_Vec2<T>& operator*=( _Vec2<T>& lhs,const _Mat2<T>& rhs )
+_Vec3<T>& operator*=(_Vec3<T>& lhs, const _Mat3<T>& rhs)
 {
 	return lhs = lhs * rhs;
 }
 
 template<typename T>
-_Vec2<T> operator*( const _Vec2<T>& lhs,const _Mat2<T>& rhs )
+_Vec3<T> operator*(const _Vec3<T>& lhs, const _Mat3<T>& rhs)
 {
-	return { 
-		lhs.x * rhs.elements[0][0] + lhs.y * rhs.elements[1][0],
-		lhs.x * rhs.elements[0][1] + lhs.y * rhs.elements[1][1]
+	return{
+		lhs.x * rhs.elements[0][0] + lhs.y * rhs.elements[1][0] + lhs.z * rhs.elements[2][0],
+		lhs.x * rhs.elements[0][1] + lhs.y * rhs.elements[1][1] + lhs.z * rhs.elements[2][1],
+		lhs.x * rhs.elements[0][2] + lhs.y * rhs.elements[1][2] + lhs.z * rhs.elements[2][2]
 	};
 }
 
-typedef _Mat2<float> Mat2;
-typedef _Mat2<double> Mad2;
+typedef _Mat3<float> Mat3;
+typedef _Mat3<double> Mad3;
